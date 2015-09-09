aef99306bc8d32f7184ba4aa191a24577d869143
Jeffrey Chau <jeffrey.chau@tigerspike.com>
2015-09-09 15:29:37 +1000

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Jeffrey Chau on 9/09/2015.
+//  Copyright (c) 2015 Tigerspike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
